439e0ce3cedbb81bd4d77716888ebd859e33f27f
unknown <cliff.mccall1@gmail.com>
2018-12-18 16:34:13 -0600

    first commit

---
+++ LearningSelenium/FirstTestClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+
+namespace LearningSelenium
+{
+    [TestClass]
+    public class FirstTestClass
+    {
+        [TestMethod]
+        public void WikiSearch()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.wikipedia.com");
+            driver.Manage().Window.Maximize();
+            List<String> textofanchors = new List<string>();
+            ReadOnlyCollection<IWebElement> anchorLists = driver.FindElements(By.TagName("a"));
+            foreach(IWebElement anchor in anchorLists)
+            {
+                textofanchors.Add(anchor.Text);
+            }
+            //driver.FindElement(By.Id("searchInput")).SendKeys("Selenium");
+            //driver.FindElement(By.ClassName("pure-button")).Click();
+            driver.Quit();
+        }
+        [TestMethod]
+        public void ChromeTestMethod()
+        {
+            string ActualResult;
+            string ExpectedResult = "Google";
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+            ActualResult = driver.Title;
+            if (ActualResult.Contains(ExpectedResult))
+            {
+                Console.WriteLine("Passed");
+                Assert.IsTrue(true, "Passed");
+            }
+            else
+            {
+                Console.Write("Failed");
+            }
+            driver.Close();
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void FireFoxTestMethod()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+            driver.Close();
+            driver.Quit();
+        }
+        [TestMethod]
+        public void IETestMethod()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.Manage().Window.Maximize();
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ LearningSelenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LearningSelenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("LearningSelenium")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("53b41c81-3b68-458d-abb6-30ae0aea5808")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
